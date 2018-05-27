27ead7ed9dbd226bb398ad2324c806b08dbbab75
Shubham_Sinha <nikhil@usedipper.com>
2018-05-27 20:51:28 +0530

    tree dictionary is a bit skewed right now

--- app.js
+++ app.js
@@ -30,6 +30,68 @@ async function sort_dict(words_dict){
 
 }
 
+function add_child(branch, key, value){
+	key = key.slice(1, );
+	var new_key = key[0]
+	branch[new_key] = {};
+	if(key.length > 1)
+	add_child(branch[new_key], key, value);
+  else{
+  branch[new_key] = [];
+	branch[new_key] = value;
+	}
+	console.log(' in add child', 'branch',branch, 'key', key, 'value', value);
+
+}
+
+function find_place_in_tree_dict(branch, key, value){
+	if(typeof branch !== 'undefined' && branch !== null){
+		add_child(branch, key, value);
+
+	}
+	else
+	{
+		while (typeof branch !== 'undefined' && branch !== null) {
+			console.log('herhe', branch, 'branch', key, 'key');
+			key = key.slice(1, );
+			new_key = key[0];
+			branch = branch[new_key];
+			console.log('branch is', branch, 'with key', new_key);
+			// statement
+		}
+		add_child(branch, key, value);
+	}
+
+
+	
+	console.log(' in finding right place child', 'branch',branch, 'key', key, 'value', value);
+
+}
+
+async function convert_to_tree(sorted_dict){
+	tree_dict = {}
+	for (var key in sorted_dict) {
+		var new_key = key[0];
+		console.log('new_key', new_key, tree_dict, key);
+		if(new_key in tree_dict)
+		{
+			console.log('new_key already prese4nt', new_key, key, sorted_dict[key], tree_dict[new_key]);
+			find_place_in_tree_dict(tree_dict[new_key], key, sorted_dict[key]);
+			
+			// recursive call to check for next alphabet, and next and so and on
+		}
+		else{
+			tree_dict[new_key] = {}
+			add_child(tree_dict[new_key], key, sorted_dict[key]);
+
+			// recursive call to enter this way {'a':{'c' :{'p':[cap], 't': [cat, act]}, ...}}
+		} 
+
+		}
+		console.log('treee', tree_dict, tree_dict['a']['c']['t']);
+	}
+
+
 
 
 app.post('/find_anagram', async function (req, res){
@@ -73,15 +135,12 @@ app.post('/find_anagram', async function (req, res){
 	words_dict = words_dict.filter(function( element ) {
    return element !== undefined;
 });
+	words_dict = words_dict.sort()
 	sorted_dict = await sort_dict(words_dict);
+	tree_dict = await convert_to_tree(sorted_dict);
 
   console.log("sorted_dict", sorted_dict, "word to search", word, "sorted word", sorted_word);
-  if(length){
-  	console.log('length is', length);
-  }
-  else {
-  	console.log('dont have length');
-  }
+  
   res.send("YO");
 });
 
